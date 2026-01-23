4d698121907fe4acf2d0ab39c9cf936c22d1efde
marvinrunge94 <marvinrunge94@gmail.com>
2026-01-23 07:11:39 +0000

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -12,12 +13,38 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [
         react(),
-        tailwindcss()
+        tailwindcss(),
+        VitePWA({
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+          manifest: {
+            name: 'Pistachio vs. Rocks',
+            short_name: 'Pistachio vs. Rocks',
+            description: 'Survive for as long as you can! Smash rocks, score points and climb the global leaderboard.',
+            theme_color: '#ffffff',
+            background_color: '#ffffff',
+            display: 'standalone',
+            icons: [
+              {
+                src: 'pwa/pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+              },
+              {
+                src: 'pwa/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable',
+              },
+            ],
+          },
+        })
       ],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
